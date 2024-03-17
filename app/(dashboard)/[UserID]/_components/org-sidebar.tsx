93d48b5d31607047ceadc59f9319c6aee20b5c77
KenG1M <tpqhenp001@gmail.com>
2024-03-17 16:34:07 +0700

    fix url favorites board

--- app/(dashboard)/[UserID]/_components/org-sidebar.tsx
+++ app/(dashboard)/[UserID]/_components/org-sidebar.tsx
@@ -77,7 +77,7 @@ export const OrgSidebar = () => {
                     className="font-normal justify-start px-2 w-full"
                 >
                     <Link href={{
-                        pathname: "/..",
+                        pathname: "../-",
                         query: { favorites: true }
                     }}>
                         <Star className="h-4 w-4 mr-2" />
