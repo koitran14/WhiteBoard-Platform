69d921f84757073fe024d71f5ae8c07aa4ddd92d
KenG1M <tpqhenp001@gmail.com>
2024-03-17 12:40:13 +0700

    Sideabar, OrgBar, NavBar

---
+++ app/(dashboard)/[UserID]/layout.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Sidebar } from"../[UserID]/_components/sidebar";
+import { OrgSidebar } from "./_components/org-sidebar";
+import { Navbar } from "./_components/navbar";
+interface DashboardLayoutProps {
+    children: React.ReactNode;
+};
+
+const DashboardLayout = ({
+    children,
+}: DashboardLayoutProps) => {
+    return (
+        <main className="h-full">
+            <Sidebar/>
+            <div className="pl-[60px] h-full">
+                <div className="flex h-full">
+                    <OrgSidebar/>
+                    <div className="h-full flex-1">
+                    <Navbar/>
+                    {children}
+
+                    </div>
+                </div>
+                
+            </div>
+        </main>
+    );
+};
+
+export default DashboardLayout;
--- app/(dashboard)/[UserID]/page.tsx
+++ app/(dashboard)/[UserID]/page.tsx
@@ -1,14 +1,23 @@
-"use client"
-import { UserButton } from "@clerk/nextjs";
-import {  useParams } from "next/navigation";
+// "use client"
+// import { UserButton } from "@clerk/nextjs";
+// import {  useParams } from "next/navigation";
 
-export default function Dashboard(){
-    const params = useParams();
+// export default function Dashboard(){ {/*dashboard==home */}
+//     const params = useParams();
 
-    return (
+//     return (
+//         <div>
+//             <p>Dashboard Page for {params.UserID}</p>
+//             <UserButton afterSignOutUrl="/"/>
+//         </div>
+//     )
+// }
+const DashboardPage=() => {
+    return(
         <div>
-            <p>Dashboard Page for {params.UserID}</p>
-            <UserButton afterSignOutUrl="/"/>
+            Dashboard Root Page
         </div>
-    )
-}
+    );
+};
+
+export default DashboardPage;
